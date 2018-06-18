52aa0d6d0b322002eb84a3e49b75aaccb18b547e
Mo Al <malbazaz@mba2018.hbs.edu>
2018-06-18 02:14:01 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,16 @@
 class Dog 
   @@all = []
   
+  attr_accessor :name 
   
+  def initialize(dog_name)
+  @dog_name = dog_name 
+  @@all << self 
+  end 
   
+  def self.all
+    puts @@all 
+  end 
   
   
   
